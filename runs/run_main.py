f90bf4a00fe9e3b6153a6318a2b419cfc9067130
qwangwl <qwangwl@gmail.com>
2026-05-11 22:23:46 +0800

    add .gitignore

--- runs/run_main.py
+++ runs/run_main.py
@@ -20,7 +20,6 @@ for sess in [1, 2, 3]:
     os.system(command)
 
 dataset_name = "seed4"
-# seed4为什么使用了lr=1e-3来着，可能是为了追更高的sota.... 实际上lr=5e-5的准确率仅相差1%左右，为了保持与论文的一致性，还是使用1e-3吧
 lr = 1e-3
 for sess in [1, 2, 3]:
     tmp_saved_path = f"logs\\{dataset_name}_{sess}_none\\DBPM"
